05c39aee04dfd734dcff7897554a63b79ca88475
Ónodi-Kiss Viktor <onodi.kiss.viktor19k@egressy.info>
2024-01-09 10:38:27 +0100

    javascript finomítások

--- pages/weboldal/js/script.js
+++ pages/weboldal/js/script.js
@@ -11,7 +11,7 @@ const kepek = ["rock-removebg-preview.png", "paerp-removebg-preview.png", "sciss
 let kivalasztott = null;
 let nyert = false;
 
-function eval(){
+async function eval(){
     let aivalaszt = null;
     switch(index2){
         case 0:
@@ -31,7 +31,7 @@ function eval(){
             break;
     }
 
-    let valaszt = kivalasztott.id;
+    let valaszt = kivalasztott;
 
     switch(aivalaszt){
         case "ko":
@@ -60,51 +60,108 @@ function eval(){
             }
             break;
     }
-    alert(nyert)
+    if(valaszt == aivalaszt){
+        $('#endTxt').css("color", "black")
+        $('#endTxt').text('Azonos választás, nincs nyertes!')
+    }
+    else{
+        if(nyert){ 
+            $('#endTxt').css("color", "darkgreen")
+            $('#endTxt').text('Nyertél!')
+        }
+        else{
+            $('#endTxt').css("color", "red")
+            $('#endTxt').text('Sajnos ez most nem sikerült!')
+        }
+    }
+    $('#endModal').modal('show');
 }
 
 function JatekMenet(req){
     kivalasztott = req;
+    index = 0;
     index2 = 0;
-    let interval = setInterval(function()
+    const interval = setInterval(function()
     {
         index2++
+        $("#aiTxt1").text("Az Ai választása: ")
         if(index2 >= kepek.length){
             index2 = 0;
         }
         if(index2 != 5 && ai.src != "../kepek/" + kepek[index2]){
             ai.src = "../kepek/" + kepek[index2]; 
+            switch(index2){
+                case 0:
+                    $("#aiTxt2").text("Kő")
+                    break;
+                case 1:
+                    $("#aiTxt2").text("Papír")
+                    break;
+                case 2:
+                    $("#aiTxt2").text("Olló")
+                    break;
+                case 3:
+                    $("#aiTxt2").text("Gyík")
+                    break;
+                case 4:
+                    $("#aiTxt2").text("Spock")
+                    break;
+            }
         }
-
-        let num = (Math.floor(Math.random()*6));
-        console.log(num)
-        if(num == 5){
+        let num = (Math.floor(Math.random()*7));
+        if(num == 6 && index > 10){
             clearInterval(interval)
             eval()
         }
 
-    }, 700)
+        index++;
 
-    
-
-    //TODO: Továbblép képpel amikor vége
-
-
-
+    }, 200)
 }
 
 koKartya.addEventListener('click', function(){
+    $("#valasztas").text("Választott kártya: Kő")
     JatekMenet("ko")
 } )
 papirKartya.addEventListener('click', function(){
+    $("#valasztas").text("Választott kártya: Papír")
     JatekMenet("papir")
 })
 olloKartya.addEventListener('click', function(){
+    $("#valasztas").text("Választott kártya: Olló")
     JatekMenet("ollo")
 })
 gyikKartya.addEventListener('click', function(){
+    $("#valasztas").text("Választott kártya: Gyík")
     JatekMenet("gyik")
 } )
 spockKartya.addEventListener('click', function(){
+    $("#valasztas").text("Választott kártya: Spock")
     JatekMenet("spock")
 } )
+
+szabalyzat.addEventListener("click", function(){
+    $("#szabalyzatModal").modal("show");
+})
+
+tortenet.addEventListener('click', function(){
+    $("#tortenetModal").modal("show");
+})
+
+$("#tortenetModal").on('hidden.bs.modal', function (e) {
+    $("#tortenetModal iframe").attr("src", $("#tortenetModal iframe").attr("src"));
+});
+
+csapat.addEventListener('click', function(){
+    $("#csapatModal").modal('show');
+})
+
+reset.addEventListener('click', function(){
+    $("#valasztas").text("Válassz kártyát!")
+
+
+    $("#aiTxt1").text("Az AI választása")
+    $("#aiTxt2").text("")
+
+    ai.src = "";
+})
